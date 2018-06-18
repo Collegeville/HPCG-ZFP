963feaf7e33ac61fa91b45e6c856dfc30267fddd
Neil Lindquist <archer1mail@gmail.com>
2018-06-18 11:39:49 -0500

    Utilize compressed matrix indices

--- src/ComputeSPMV.cpp
+++ src/ComputeSPMV.cpp
@@ -19,7 +19,17 @@
  */
 
 #include "ComputeSPMV.hpp"
-#include "ComputeSPMV_ref.hpp"
+
+#ifndef HPCG_NO_MPI
+#include "ExchangeHalo.hpp"
+#endif
+
+#ifndef HPCG_NO_OPENMP
+#include <omp.h>
+#endif
+#include <cassert>
+#include "DecodeNextIndex.hpp"
+
 
 /*!
   Routine to compute sparse matrix vector product y = Ax where:
@@ -39,7 +49,33 @@
 */
 int ComputeSPMV( const SparseMatrix & A, Vector & x, Vector & y) {
 
-  // This line and the next two lines should be removed and your version of ComputeSPMV should be used.
-  A.isSpmvOptimized = false;
-  return ComputeSPMV_ref(A, x, y);
+  assert(x.localLength>=A.localNumberOfColumns); // Test vector lengths
+  assert(y.localLength>=A.localNumberOfRows);
+
+#ifndef HPCG_NO_MPI
+    ExchangeHalo(A,x);
+#endif
+  const double * const xv = x.values;
+  double * const yv = y.values;
+  const local_int_t nrow = A.localNumberOfRows;
+
+  local_int_t indexId = 0;
+  local_int_t uCount = 0;
+  local_int_t curCol = -20;
+
+//#ifndef HPCG_NO_OPENMP
+//  #pragma omp parallel for
+//#endif
+  for (local_int_t i=0; i< nrow; i++)  {
+    double sum = 0.0;
+    const double * const cur_vals = A.matrixValues[i];
+    const int cur_nnz = A.nonzerosInRow[i];
+
+    for (int j=0; j< cur_nnz; j++){
+      DecodeNextIndex_Forward(A, indexId, uCount, curCol);
+      sum += cur_vals[j]*xv[curCol];
+    }
+    yv[i] = sum;
+  }
+  return 0;
 }
--- src/ComputeSYMGS.cpp
+++ src/ComputeSYMGS.cpp
@@ -19,7 +19,11 @@
  */
 
 #include "ComputeSYMGS.hpp"
-#include "ComputeSYMGS_ref.hpp"
+#ifndef HPCG_NO_MPI
+#include "ExchangeHalo.hpp"
+#endif
+#include <cassert>
+#include "DecodeNextIndex.hpp"
 
 /*!
   Routine to one step of symmetrix Gauss-Seidel:
@@ -49,7 +53,57 @@
 */
 int ComputeSYMGS( const SparseMatrix & A, const Vector & r, Vector & x) {
 
-  // This line and the next two lines should be removed and your version of ComputeSYMGS should be used.
-  return ComputeSYMGS_ref(A, r, x);
+  assert(x.localLength==A.localNumberOfColumns); // Make sure x contain space for halo values
 
+#ifndef HPCG_NO_MPI
+  ExchangeHalo(A,x);
+#endif
+
+  const local_int_t nrow = A.localNumberOfRows;
+  double ** matrixDiagonal = A.matrixDiagonal;  // An array of pointers to the diagonal entries A.matrixValues
+  const double * const rv = r.values;
+  double * const xv = x.values;
+
+  local_int_t indexId = 0;
+  local_int_t uCount = 0;
+  local_int_t curCol = -20;
+
+  for (local_int_t i=0; i< nrow; i++) {
+    const double * const currentValues = A.matrixValues[i];
+    const int currentNumberOfNonzeros = A.nonzerosInRow[i];
+    const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
+    double sum = rv[i]; // RHS value
+
+    for (int j=0; j< currentNumberOfNonzeros; j++) {
+      DecodeNextIndex_Forward(A, indexId, uCount, curCol);
+      sum -= currentValues[j] * xv[curCol];
+    }
+    sum += xv[i]*currentDiagonal; // Remove diagonal contribution from previous loop
+
+    xv[i] = sum/currentDiagonal;
+
+  }
+
+  // Now the back sweep.
+
+  indexId = 0;
+  uCount = 0;
+  curCol = -20;
+
+  for (local_int_t i=nrow-1; i>=0; i--) {
+    const double * const currentValues = A.matrixValues[i];
+    const int currentNumberOfNonzeros = A.nonzerosInRow[i];
+    const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
+    double sum = rv[i]; // RHS value
+
+    for (int j = 0; j< currentNumberOfNonzeros; j++) {
+      DecodeNextIndex_Backward(A, indexId, uCount, curCol);
+      sum -= currentValues[j]*xv[curCol];
+    }
+    sum += xv[i]*currentDiagonal; // Remove diagonal contribution from previous loop
+
+    xv[i] = sum/currentDiagonal;
+  }
+
+  return 0;
 }
